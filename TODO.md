4d5c6809197a8efec5581d5a5b66a6cc87127dba
Gerald Adarve <gerald.adarve@stepstone.com>
2022-11-09 16:34:52 +0000

    Implemented use of param() through out, updated help menu variable, renamed tfflick worked function argument variable, changed default columns in menu function

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 ## To-Do
 
+* Currently working on improving the menu option - The no argument option of `tfflick`. It displays ok when the Powershell window is fully expanded. If the window is not in full mode, the top part of the list is not visible, in this case is better to use the option `tfflick {version number}` 
 * Make available for Windows 32 bit architecture - availability to download the 32 bit version of the Terraform executable
 * Provided Un-installer option
 * Improve menu option - The no argument option of `tfflick`
--- tfflick/tfflick.psm1
+++ tfflick/tfflick.psm1
@@ -1,9 +1,16 @@
 ﻿Function tfflick {
+    Param(
+        [Parameter(Mandatory=$false, Position=0, HelpMessage="Argument to make a decision")][string] $argument,
+        [Parameter(Mandatory=$false, HelpMessage="Help menu argument")][switch] $h
+    )
+
 # Credits
 # Create-Menu function developed by JDDellGuy at https://community.spiceworks.com/people/josiahdeal3479
 # https://community.spiceworks.com/scripts/show/4785-create-menu-2-0-arrow-key-driven-powershell-menu-for-scripts
 # Renamed the function to __tfflick_menu
-# Un-initialised variable $Columns and set the value in the main function body - $Columns = "Auto"
+try {
+    # Set -h argument to present help options menu
+    if ($h) {$argument = "help"}
 Function __tfflick_menu (){
     #Start-Transcript "C:\_RRC\MenuLog.txt"
     Param(
@@ -14,7 +21,7 @@ Function __tfflick_menu (){
         [Parameter(Mandatory=$False)][bool]$ShowCurrentSelection=$False
     )
 
-    $Columns = "Auto"
+    # $Columns = 8
     $MaxValue = $MenuOptions.count-1
     $Selection = 0
     $EnterPressed = $False
@@ -158,22 +165,18 @@ Function __tfflick_menu (){
     }
 }
 
-try {    
-    # Accept version number as input parameter
-    $argument = $args[0]      
-    Function __tfflick_worker {
+#********************** tfflick **********************
 
+# tfflick code starts here    
+    Function __tfflick_worker {    
         Param(
-            $argument,
-            $tfversions,
-            $versionfile,
-            $tfflickpath
+            [Parameter(Mandatory=$True, Position=0, HelpMessage="Argument to make a decision")]$tfw_argument            
         )
 
         $homedir     = $env:USERPROFILE
         $tfflickpath = $homedir+"\.tfflick"
         $tfversions  = $tfflickpath+"\tfversions"    
-        $versionfile = "terraform_"+$argument+".exe" # Set version format as the file downloads as terraform.exe
+        $versionfile = "terraform_"+$tfw_argument+".exe" # Set version format as the file downloads as terraform.exe
 
         # Create tfflick and tfversions working directories to hold all downloaded Terraform versions if it doesn't exist    
         if (-not(Test-Path -Path $tfflickpath)) {
@@ -182,19 +185,19 @@ try {
         }
 
         # Set url and zip file format
-        $url = "https://releases.hashicorp.com/terraform/"+$argument+"/terraform_"+$argument+"_windows_amd64.zip"
-        $zipfile = "terraform_"+$argument+"_windows_amd64.zip"
+        $url = "https://releases.hashicorp.com/terraform/"+$tfw_argument+"/terraform_"+$tfw_argument+"_windows_amd64.zip"
+        $zipfile = "terraform_"+$tfw_argument+"_windows_amd64.zip"
           
         # Check if file files already exists. If it does, don't download.
         if (Test-Path -Path $tfversions"\"$versionfile) {
-           Write-Host "Version $argument already downloaded"
+           Write-Host "Version $tfw_argument already downloaded"
         }
         elseif ( -not(Test-Path -Path $tfversions"\"$versionfile))
         {
         # Download file if not already in the system
            $ProgressPreference = 'SilentlyContinue'       
            Invoke-WebRequest $url -OutFile $tfflickpath"\"$zipfile
-           Write-Host "Downloading version $argument"
+           Write-Host "Downloading version $tfw_argument"
            Expand-Archive $tfflickpath"\"$zipfile -DestinationPath $tfversions -Force
            Rename-Item -Path $tfversions"\terraform.exe" -NewName $versionfile -Force       
            Remove-Item -Path $tfflickpath"\"$zipfile -Force
@@ -203,7 +206,7 @@ try {
         New-Item -ItemType HardLink -Path $tfflickpath -Name terraform.exe -Value $tfversions"\"$versionfile -Force
     }
 
-    if  (  -not($argument)) { # Present full list of versions if no argument is provided
+    if  ( -not($argument)) { # Present full list of versions if no argument is provided
         
         # Retrieve list of available Terraform versions
         $ProgressPreference = 'SilentlyContinue'
@@ -213,34 +216,34 @@ try {
         } | Select-Object outerText
         
         # Create list of options to be presented to user. Format: 1.3.4 - terraform_1.3.4
-        $shortversionslist = foreach ($i in $list.outerText) {    
-            $i.Substring("terraform_".Length, $i.Length-"terraform_".Length)
-        }
+        $shortversionslist =  $list.outerText | ForEach-Object -Process {$_.Substring("terraform_".Length, $_.Length-"terraform_".Length)}
 
         # Display menu list of all available Terraform versions
-        $Title = " tfflick `n Select the desired Terraform version and press enter. `n"
+        $Title = "#### tfflick ####`n Select the desired Terraform version and press enter. `n This is the argument $argument"
         $Options = $shortversionslist
-        $Selection = __tfflick_menu -MenuTitle $Title -MenuOptions $Options -Columns "auto" -MaximumColumnWidth 20 -ShowCurrentSelection $True
+        $Selection = __tfflick_menu -MenuTitle $Title -MenuOptions $Options -Columns 12 -MaximumColumnWidth 20 -ShowCurrentSelection $True
         Write-Host "You selected version " $shortversionslist[$Selection]
         $argument = $shortversionslist[$Selection]
 
-        __tfflick_worker -argument $argument
+        __tfflick_worker -tfw_argument $argument
     }
     elseif ($argument -match "^[0-9]+.[0-9]+.[0-9]+$") {
-        __tfflick_worker -argument $argument
+        __tfflick_worker -tfw_argument $argument
     }        
-    elseif ($argument -like "-h" -or $argument -like "help") { # Display tfflick usage options
-        $tfflickusage = "## Usage
+    elseif ($argument -eq "help") { # Display tfflick usage options
+        $tfflickusage = @'
+        ## Usage
 
         * tfflick (no arguments)
-        - Returns a menu list of available Terraform versions
-        - Use your arrow keys to select the desired version and press enter
+          - Returns a menu list of available Terraform versions
+          - Use your arrow keys to select the desired version and press enter
         
         * tfflick {version number}
-        - To flick to a specific version number, pass the desired version as the argument. For example tfflick 1.3.4 
+          - To flick to a specific version number, pass the desired version as the argument. For example tfflick 1.3.4 
 
         * tfflick -h or tfflick help
-        - Displays tfflick usage options `n"
+          - Displays tfflick usage options
+'@
         
         Write-Host $tfflickusage
 
