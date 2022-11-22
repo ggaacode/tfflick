ec04d559c81f64648ce16696d929d0ac8cd30bb1
Gerald Adarve <gadarve@hotmail.com>
2022-11-22 15:46:31 +0000

    saving changes

--- testing-new-menu.ps1
+++ testing-new-menu.ps1
@@ -1,117 +1,136 @@
+
+Function __tfflick_shortmenu (){
+      
+    Param(
+        [Parameter(Mandatory=$True)][String]$MenuTitle,
+        [Parameter(Mandatory=$True)][array]$MenuOptions,
+        [Parameter(Mandatory=$True)][String]$RowsToDisplay,
+        [Parameter(Mandatory=$False)][bool]$ShowCurrentSelection=$False    )
+
+
+$RowsToDisplay = 5
+$Start = 0
+$End = $RowsToDisplay-1
+$Rows = $MenuOptions.count
+$Selection = 0
+$DownArrow = [char]25
+$UpArrow = [char]24
+$EnterPressed = $False
+
+Clear-Host
+    
+While($EnterPressed -eq $False){    
+    
+    Write-Host "$MenuTitle"
+
+    If ($ShowCurrentSelection -eq $True){
+        $Host.UI.RawUI.WindowTitle = "CURRENT SELECTION: $($MenuOptions[$Selection])"
+    }
+
+    for ($i=$Start; $i -le $End; $i++) {
+        if ($i -eq $Selection) {
+            if ($i -eq $End -and $End -ne $Rows-1) {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i] $DownArrow
+            }
+            elseif ($i -eq $Start -and $Start -ne 0) {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i] $UpArrow
+            }
+            else {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i]
+            }
+        }
+        else
+        {
+            if ($i -eq $End -and $End -ne $Rows-1) {
+                Write-Host "`t"$MenuOptions[$i] $DownArrow
+            }
+            elseif ($i -eq $Start -and $Start -ne 0) {
+                Write-Host "`t"$MenuOptions[$i] $UpArrow
+            }
+            else {
+                Write-Host "`t"$MenuOptions[$i]
+            }
+        }
+    }        
+        
+    $KeyInput = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown").virtualkeycode
+
+       Switch($KeyInput){
+           13{
+               $EnterPressed = $True
+               Return $Selection          
+               Clear-Host
+               break
+           }           
+           38{ #Up
+               if ($Selection -gt 0 -and $Selection -le ($Rows-1)) {
+                   if ($Selection -eq $End) {
+                       $Selection -= 1
+                       $Start = $Start
+                       $End = $End
+                   }
+                   elseif($Selection -gt $Start -and $Selection -lt $End) {
+                       $Selection -= 1
+                       $Start = $Start
+                       $End = $End
+                   }
+                   elseif ($Selection -le $Start) {                   
+                       $Selection -= 1
+                       $Start -= 1
+                       $End -= 1
+                   }
+               }                          
+               Clear-Host
+               break
+           }
+
+           40{ #Down
+               if ($Selection -ge 0 -and $Selection -lt ($Rows-1)) {
+                  if ($Selection -eq $Start) {
+                   $Selection += 1
+                   $Start = $Start
+                   $End = $End
+                  }
+                  elseif($Selection -gt $Start -and $Selection -lt $End) {
+                   $Selection += 1
+                   $Start = $Start
+                   $End = $End
+                  }
+                  elseif ($Selection -ge $End) {                   
+                   $Selection += 1
+                   $Start += 1
+                   $End += 1
+                  }
+               }                       
+               Clear-Host
+               break
+           }            
+       
+           Default{
+               Clear-Host
+           }
+        }
+}    
+}
+
 $baseurl = "https://releases.hashicorp.com/terraform/"
 
 $ProgressPreference = 'SilentlyContinue'
-        $versionslist = Invoke-WebRequest -URI $baseurl
-        $list = $versionslist.Links | Where-Object {
+$versionslist = Invoke-WebRequest -URI $baseurl
+$list = $versionslist.Links | Where-Object {
             $_.outerText -match "^terraform_[0-9]+.[0-9]+.[0-9]+$" -and $_.outerText -notlike "*_0.1.*"
         } | Select-Object outerText
         
-        # Create list of options to be presented to user. Format: 1.3.4 - terraform_1.3.4
-        $shortversionslist =  $list.outerText | ForEach-Object -Process {$_.Substring("terraform_".Length, $_.Length-"terraform_".Length)}
-
+# Create list of options to be presented to user. Format: 1.3.4 - terraform_1.3.4
+$shortversionslist =  $list.outerText | ForEach-Object -Process {$_.Substring("terraform_".Length, $_.Length-"terraform_".Length)}
         
-        $Options = $shortversionslist
-        #$Options[0..4]
+$Title = "     ## tfflick ##`nSelect a Terraform version `nand press enter.`n"
+$Options = $shortversionslist
 
-        #$Options[0..4] | ForEach-Object -Process {Write-Host $_}
+$Selection = __tfflick_shortmenu -MenuTitle $Title -MenuOptions $Options -RowsToDisplay 5 -ShowCurrentSelection $True
+Clear-Host
+Write-Host "You selected version " $shortversionslist[$Selection]
+$argument = $shortversionslist[$Selection]
 
-        $RowsIndex = 5
-        $Start = 0
-        $End = $RowsIndex-1
-        $Rows = $Options.count
-        $IndexGuide = 2
-        $IndexTail = $RowsIndex-$IndexGuide
-       # $Options[186]
-
-        $Selection = 0
-
-        
-
-        #$Options[0..4]
-    $EnterPressed = $False
-        Clear-Host
-    
-    While($EnterPressed -eq $False){         
-
-        Write-Host -ForegroundColor Red "$Selection"
-        Write-Host -ForegroundColor Red "This is Start $Start"
-        Write-Host -ForegroundColor Red "This is End $End"
-        Write-Host -ForegroundColor Red "This is Rows $Rows"
-
-        for ($i=$Start; $i -le $End; $i++) {
-            if ($i -eq $Selection) {
-                Write-Host -BackgroundColor White -ForegroundColor Black $Options[$i]
-            }
-            else
-            {
-                Write-Host $Options[$i]
-            }
-        }
-        Write-Host -ForegroundColor Green "$Selection"
-        Write-Host -ForegroundColor Green "This is Start $Start"
-        Write-Host -ForegroundColor Green "This is End $End"
-        Write-Host -ForegroundColor Red "This is Rows $Rows"
-        
-         $KeyInput = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown").virtualkeycode
-
-        Switch($KeyInput){
-            13{
-                $EnterPressed = $True
-                Return $Selection          
-                Clear-Host
-                break
-            }           
-
-            38{ #Up
-                if ($Selection -ge $RowsIndex-1 -and $Selection -lt ($Rows-1)) {
-                    $Selection -= 1
-                    $Start -= 1
-                    $End -= 1
-                }           
-                elseif ($Selection -gt 0 -and $Selection -lt $RowsIndex-1) {
-                    $Selection -= 1
-                    $Start -= 1
-                    $End -= 1
-                }           
-                Clear-Host
-                break
-            }
-
-            40{ #Down
-                # if ($Selection -ge 0 -and $Selection -lt $RowsIndex-1) {
-                #     $Selection += 1
-                #     $Start = 0
-                #     $End = $RowsIndex-1
-                # }
-                # elseif ($Selection -ge 0 -and $Selection -lt ($Rows-1)) {
-                #     $Selection += 1
-                #     $Start += 1
-                #     $End += 1
-                # }         
-                if ($Selection -ge 0 -and $Selection -lt ($Rows-1)) {
-                   if ($Selection -eq $Start) {
-                    $Selection += 1
-                    $Start = $Start
-                    $End = $End
-                   }
-                   elseif($Selection -gt $Start -and $Selection -le $End) {
-                    $Selection += 1
-                    $Start = $Start
-                    $End = $End
-                   }
-                   elseif ($Selection -gt $End) {                   
-                    $Selection += 1
-                    $Start += 1
-                    $End += 1
-                   }
-                }
-                       
-                Clear-Host
-                break
-            }            
-        
-            Default{
-                Clear-Host
-            }
-    }
-}    
+# Call __tfflick_worker function with selected version
+#__tfflick_worker -tfw_argument $argument 
--- tfflick/tfflick.psm1
+++ tfflick/tfflick.psm1
@@ -72,38 +72,13 @@ Function __tfflick_menu (){
     
     Clear-Host
 
-    # $RowsIndex = 5
-    # $RowsStart = 0
-    # $RowsEnd = $RowQty-1
-
     While($EnterPressed -eq $False){ 
 
         Write-Host "$MenuTitle"
         
         If ($ShowCurrentSelection -eq $True){
            $Host.UI.RawUI.WindowTitle = "CURRENT SELECTION: $($MenuOptions[$Selection])"
-        }
-
-        $MenuListing = $MenuListing[1..5]
-        $RowQty = 5
-        # $i = 2     
-               
-        # If ($Selection -lt $RowQty-1) {
-        #    $i = 0
-        #    $RowQty = 5           
-        # }
-        # ElseIf ($Selection -ge $RowQty-1) {
-        #     # $RowsStart = $Selection-($RowQty-1)
-        #     # $RowsEnd   = $Selection
-
-        #     $i = $Selection-($RowQty-1)
-        #     $RowQty = $Selection+1
-        # }
-
-       # $MenuListing = $MenuListing[$RowsStart..$RowsEnd]
-
-        Write-Host "This is ii $i"
-        Write-Host "This is RowQty $RowQty"
+        }        
                
         Write-Host -ForegroundColor Red "$Selection"
 
@@ -190,6 +165,118 @@ Function __tfflick_menu (){
     }
 }
 
+Function __tfflick_shortmenu (){
+      
+    Param(
+        [Parameter(Mandatory=$True)][String]$MenuTitle,
+        [Parameter(Mandatory=$True)][array]$MenuOptions,
+        [Parameter(Mandatory=$True)][String]$RowsToDisplay,
+        [Parameter(Mandatory=$False)][bool]$ShowCurrentSelection=$False)
+
+$RowsToDisplay = 5
+$Start = 0
+$End = $RowsToDisplay-1
+$Rows = $MenuOptions.count
+$Selection = 0
+$DownArrow = [char]25
+$UpArrow = [char]24
+$EnterPressed = $False
+
+Clear-Host
+    
+While($EnterPressed -eq $False){    
+    
+    Write-Host "$MenuTitle"
+
+    If ($ShowCurrentSelection -eq $True){
+        $Host.UI.RawUI.WindowTitle = "CURRENT SELECTION: $($MenuOptions[$Selection])"
+    }
+
+    for ($i=$Start; $i -le $End; $i++) {
+        if ($i -eq $Selection) {
+            if ($i -eq $End -and $End -ne $Rows-1) {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i] $DownArrow
+            }
+            elseif ($i -eq $Start -and $Start -ne 0) {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i] $UpArrow
+            }
+            else {
+                Write-Host -BackgroundColor White -ForegroundColor Black "`t"$MenuOptions[$i]
+            }
+        }
+        else
+        {
+            if ($i -eq $End -and $End -ne $Rows-1) {
+                Write-Host "`t"$MenuOptions[$i] $DownArrow
+            }
+            elseif ($i -eq $Start -and $Start -ne 0) {
+                Write-Host "`t"$MenuOptions[$i] $UpArrow
+            }
+            else {
+                Write-Host "`t"$MenuOptions[$i]
+            }
+        }
+    }        
+        
+    $KeyInput = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown").virtualkeycode
+
+       Switch($KeyInput){
+           13{
+               $EnterPressed = $True
+               Return $Selection          
+               Clear-Host
+               break
+           }           
+           38{ #Up
+               if ($Selection -gt 0 -and $Selection -le ($Rows-1)) {
+                   if ($Selection -eq $End) {
+                       $Selection -= 1
+                       $Start = $Start
+                       $End = $End
+                   }
+                   elseif($Selection -gt $Start -and $Selection -lt $End) {
+                       $Selection -= 1
+                       $Start = $Start
+                       $End = $End
+                   }
+                   elseif ($Selection -le $Start) {                   
+                       $Selection -= 1
+                       $Start -= 1
+                       $End -= 1
+                   }
+               }                          
+               Clear-Host
+               break
+           }
+
+           40{ #Down
+               if ($Selection -ge 0 -and $Selection -lt ($Rows-1)) {
+                  if ($Selection -eq $Start) {
+                   $Selection += 1
+                   $Start = $Start
+                   $End = $End
+                  }
+                  elseif($Selection -gt $Start -and $Selection -lt $End) {
+                   $Selection += 1
+                   $Start = $Start
+                   $End = $End
+                  }
+                  elseif ($Selection -ge $End) {                   
+                   $Selection += 1
+                   $Start += 1
+                   $End += 1
+                  }
+               }                       
+               Clear-Host
+               break
+           }            
+       
+           Default{
+               Clear-Host
+           }
+        }
+}    
+}
 #********************** tfflick **********************
 
 # tfflick code starts here  
@@ -245,12 +332,11 @@ $baseurl = "https://releases.hashicorp.com/terraform/"
         
         # Create list of options to be presented to user. Format: 1.3.4 - terraform_1.3.4
         $shortversionslist =  $list.outerText | ForEach-Object -Process {$_.Substring("terraform_".Length, $_.Length-"terraform_".Length)}
-
-        # Display menu list of all available Terraform versions
-        $Title = "#### tfflick ####`n Select the desired Terraform version and press enter. `n This is the argument $argument"
-        $Options = $shortversionslist
         
-        $Selection = __tfflick_menu -MenuTitle $Title -MenuOptions $Options -Columns 1 -MaximumColumnWidth 20 -ShowCurrentSelection $True
+        $Title = "     ## tfflick ##`nSelect a Terraform version `nand press enter.`n"
+        $Options = $shortversionslist
+
+        $Selection = __tfflick_shortmenu -MenuTitle $Title -MenuOptions $Options -RowsToDisplay 5 -ShowCurrentSelection $True
         Clear-Host
         Write-Host "You selected version " $shortversionslist[$Selection]
         $argument = $shortversionslist[$Selection]
