1550c43f16bf6c524ddb987055ab573b1f740432
Gerald Adarve <gerald.adarve@stepstone.com>
2022-11-23 09:37:13 +0000

    saving changes

--- run_before_commit.ps1
+++ run_before_commit.ps1
@@ -3,6 +3,6 @@ terraform-docs.exe .
 $compress = @{
     Path = ".\tfflick", ".\install_tfflick.ps1", ".\README.md"
     CompressionLevel = "Fastest"
-    DestinationPath = ".\tfflick_v1.0.0.zip"
+    DestinationPath = ".\tfflick_v1.0.0_shortmenu.zip"
   }
   Compress-Archive @compress -Force
