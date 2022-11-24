cef8d10cfa9b9956783d6020b7e27697606ca476
Gerald Adarve <gerald.adarve@stepstone.com>
2022-11-24 17:17:26 +0000

    removing file in preparation for release

--- run_before_commit.ps1
+++
@@ -1,8 +0,0 @@
-terraform-docs.exe .
-
-$compress = @{
-    Path = ".\tfflick", ".\install_tfflick.ps1", ".\README.md"
-    CompressionLevel = "Fastest"
-    DestinationPath = ".\tfflick_v1.0.0_shortmenu.zip"
-  }
-  Compress-Archive @compress -Force
